0a82ad827048b2aa21ad2ac31928745c7bb288a8
Daniel Noble <daniel.noble@anu.edu.au>
2022-09-16 09:37:02 +1000

    Cleaning data up to make it just a little bit easier for students. Added some code to wrangle.r

--- data/Assignment2/wrangle_clark_data.R
+++ data/Assignment2/wrangle_clark_data.R
@@ -1,9 +1,18 @@
 
-
+    dat_OA <- read_csv("./data/Assignment2/OA_activitydat_20190302_BIOL3207.csv")
 clark_meta <- read_csv("./data/Assignment2/clark_paper_data.csv")
+
+summary_data <-dat_OA %>% group_by(species, treatment) %>% summarise(mean = mean(activity, na.rm = TRUE), sd = sd(activity, na.rm = TRUE), n = length(unique(animal_id)))
 
 total <- cbind(clark_meta, summary_data)
 
-final <- pivot_wider(total, names_from = Treatment,
-                     names_glue = "{Treatment}_{.value}",
-                     values_from = c("Mean Activity", "Standard Error", "N_inds", "N"))
+final <- pivot_wider(total, names_from = treatment,
+                     names_glue = "{treatment}_{.value}",
+                     values_from = c("mean", "sd", "n"))
+
+meta_data_full <- read_csv("./data/Assignment2/ocean_meta_data.csv")
+
+dim(meta_data_full)
+dim(final)
+
+
